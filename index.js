802197f32030afbf14a9399d35709bf90e5272d2
Lydando <seanvedanayagam@gmail.com>
2025-10-23 21:32:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -165,3 +165,10 @@ app.use((req, res, next) => {
     log(`serving on port ${port}`);
   });
 })();
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  });
+}
